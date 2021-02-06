ee491c464b654a1f24d573d1c136448939b1bbbc
Andrew Cassin <acassin999@gmail.com>
2021-02-06 16:37:03 +1100

    Add implementation of app name and version also using git cli

--- health/health/views.py
+++ health/health/views.py
@@ -10,18 +10,28 @@ def git_hash():
    hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
    return hash
 
-def app_name(request):
+def app_name():
    """
        Return django app name to caller
    """
    appname = subprocess.check_output("basename -s .git `git config --get remote.origin.url`", shell=True).strip()
    return appname
 
+def git_most_recent_tag():
+   """
+       Return the most recent git release tag associated with the repo and return to caller
+   """
+   version = subprocess.check_output(["git", "describe", "--exact-match", "--abbrev=0"])
+   return version
+
 @api_view(['GET'])
 def git_view(request):
+   """
+      Implement a function-based view using Django REST framework. Really simple implementation.
+   """
    data = {
       "git hash": git_hash(),
-      "app name": app_name(request),
-      "app version": "ZZZZ"
+      "app name": app_name(),
+      "app version": git_most_recent_tag(),
    }
    return Response(data)
