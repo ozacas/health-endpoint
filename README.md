6d577a9ffbc03eb5e721e30b61a391bfad7a488b
Andrew Cassin <acassin999@gmail.com>
2021-02-06 15:38:59 +1100

    Use git describe to obtain HEAD hash from cloned repo. Started README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # health-endpoint
 Return health status as an API endpoint
+
+## Executing unit tests
+
+python3 manage.py test
+
+## Building docker container
+
+~~~~
+   git clone https://github.com/ozacas/health-endpoint.git
+   cd health-endpoint
+   docker build deploy
+   export RELEASE=ozacas/health-endpoint:v0.1.5
+   docker tag `docker images -q | head -n 1` $RELEASE
+~~~~
+
+## Publish to eg. docker hub
+
+~~~~
+   docker push $RELEASE
+~~~~
+
+## Running docker container
+
+~~~~
+   docker run -p 8000:8000 $RELEASE
+   # after a few seconds to start the container, browse to http://localhost:8000/health
+~~~~
+
--- health/health/views.py
+++ health/health/views.py
@@ -1,12 +1,27 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import subprocess
+
+def git_hash():
+   """
+       Use git client to obtain latest commit hash for main branch and returns it
+   """
+   hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
+   return hash
+
+def app_name(request):
+   """
+       Return django app name to caller
+   """
+   appname = request.resolver_match.app_name
+   return appname
 
 @api_view(['GET'])
 def git_view(request):
    data = {
-      "git hash": "XXX",
-      "app name": "XXX",
+      "git hash": git_hash(),
+      "app name": app_name(request),
       "app version": "ZZZZ"
    }
    return Response(data)
