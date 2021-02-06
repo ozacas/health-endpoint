a9674d3243a92d88c22cfd1e7e7d46c489fddd0f
Andrew Cassin <acassin999@gmail.com>
2021-02-07 09:01:19 +1100

    Fix missing import and flake8

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 __version__ = "0.2.0"
 
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 TEST_REQUIREMENTS = [
-    'pytest',
-    'pytest-django',
-    'pylint',
-    'pylint_django',
+    "pytest",
+    "pytest-django",
+    "pylint",
+    "pylint_django",
 ]
 
-setuptools.setup(
-    name="health-endpoint", 
+setup(
+    name="health-endpoint",
     version=__version__,
     author="ozacas",
     author_email="https://github.com/ozacas",
@@ -21,15 +21,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ozacas/health-endpoint",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires=">=3.5",
     tests_require=TEST_REQUIREMENTS,
-    package_dir={'': 'src'},  
+    package_dir={"": "src"},
 )
-
-
